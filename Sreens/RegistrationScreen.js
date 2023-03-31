c4c44b64412fed52f8cfc7b410f635df03cecd04
BohdanHromchak <gromchakbogdan@gmail.com>
2023-03-31 22:11:05 +0300

    Update RegistrationScreen.js

--- Sreens/RegistrationScreen.js
+++ Sreens/RegistrationScreen.js
@@ -9,7 +9,8 @@ import {
   Platform,
   Button,
   Text,
-  ImageBackground,
+  Image,
+  Pressable,
 } from "react-native";
 
 export default function RegistrationScreen() {
@@ -30,42 +31,45 @@ export default function RegistrationScreen() {
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.container}>
+      <View>
         <KeyboardAvoidingView
           behavior={Platform.OS == "ios" ? "padding" : "height"}
         >
-          <ImageBackground
-            source={require("../assets/screenBg.png")}
-            style={styles.image}
-          >
-              <View style={styles.formWrap}>
-            <Text style={styles.title}>Регистрация</Text>
-            <TextInput
-              value={login}
-              onChangeText={loginHandler}
-              placeholder="Логин"
-              style={styles.input}
+          <View style={styles.container}>
+            <Image
+              source={require("../assets/screenBg.png")}
+              style={styles.image}
             />
-            <TextInput
-              value={email}
-              onChangeText={emailHandler}
-              placeholder="Адрес электронной почты"
-              style={styles.input}
-            />
-            <TextInput
-              value={password}
-              onChangeText={passwordHandler}
-              placeholder="Пароль"
-              secureTextEntry={true}
-              style={styles.input}
-            />
-            <Button
-              title={"Зарегистрироваться"}
-              style={styles.input}
-              onPress={onLogin}
-            />
-             </View>
-          </ImageBackground>
+            <View style={styles.formWrap}>
+              <Text style={styles.title}>Регистрация</Text>
+
+              <View style={styles.inputWrap}>
+              <TextInput
+                value={login}
+                onChangeText={loginHandler}
+                placeholder="Логин"
+                style={styles.input}
+              />
+              <TextInput
+                value={email}
+                onChangeText={emailHandler}
+                placeholder="Адрес электронной почты"
+                style={styles.input}
+              />
+              <TextInput
+                value={password}
+                onChangeText={passwordHandler}
+                placeholder="Пароль"
+                secureTextEntry={true}
+                style={styles.input}
+              />
+              </View>
+              <Pressable onPress={onLogin} style={styles.button}>
+                <Text style={styles.buttonText}>Зарегистрироваться</Text>
+              </Pressable>
+              <Text>Уже есть аккаунт? Войти</Text>
+            </View>
+          </View>
         </KeyboardAvoidingView>
       </View>
     </TouchableWithoutFeedback>
@@ -74,27 +78,48 @@ export default function RegistrationScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    // flexDirection: 'row',
-    flex: 1,
-    // flexDirection: "row",
-    // height: "100%",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    height: "100%",
+    width: "100%",
+  },
+  formWrap: {
+    // backgroundColor: "#FFFFFF",
+    // borderRadius: '25 25 0 0',
+    // width: "100%",
+    // alignItems: 'center',
+    // justifyContent: 'flex-end'
+    // top: 200,
+    // height: 370,
+    // flexDirection: "column",
     // alignItems: "center",
     // justifyContent: "center",
-    // backgroundColor: "red",
+    width: '100%',
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+  inputWrap: {
+    flexDirection:'column', gap: 16,
+    marginBottom: 43,
   },
   image: {
-    height: '100%',
-    width: '100%',
-    resizeMode: "cover",
-
-    // flexDirection: 'row',
-    // height: '100%',
-    // flex: 1,
-    // position: absolute,
-    // position: 'absolute',
-    // width: 375,
-    // height: '100%',
-    // height: "100%",
-    // width: "100%",
+    position: "absolute",
+    width: "100%",
+    top: 0,
+  },
+  input: {
+    height: 40,
+    padding: 10,
+    borderWidth: 1,
+    backgroundColor: "#F6F6F6",
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+  },
+  button: {
+    color: "#FFFFFF",
+    backgroundColor: "#FF6C00",
+  },
+  buttonText: {
+    color: "#FFFFFF",
   },
 });
